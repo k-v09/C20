5ad54d64656303d7dd145595eebb9071ada359a5
Noah Sehman <noah.sehman@gmail.com>
2024-06-12 20:36:35 -0700

    oopsies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer><ul>
     <li><a href="https://github.com/k-v09">GitHub</a></li>
-    <li><a href="linkedin.com/in/noah-sehman-1a6494258">LinkedIn</a></li>
+    <li><a href="https://linkedin.com/in/noah-sehman-1a6494258">LinkedIn</a></li>
     <li><a href="https://www.instagram.com/namheshaon/">Instagram</a></li>
   </ul></footer>
 );
